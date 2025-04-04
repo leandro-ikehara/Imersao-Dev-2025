47fbdc60cbf089dbc39e0f37dc60f9a2d255c58a
leandro-ikehara <leandro.ikehara@gmail.com>
2025-04-04 10:32:52 -0300

    Laço FOR

--- AULA3/rodadas.js
+++ AULA3/rodadas.js
@@ -1,19 +1,19 @@
-rodada = 1
-while(rodada <= 3) {
+venceu = 0
+
+for (let rodada = 1; rodada <= 3; rodada++) {
     console.log("Rodada: " + rodada)
 
     escolhaJogador = prompt("Nível " + rodada + ", vidro (1-3): ")
     pisoQuebrado = Math.floor(Math.random() * 3) + 1;
 
-    if(escolhaJogador == pisoQuebrado) {
-        alert("O vidro quebrou! Fim de jogo!")
+    if (escolhaJogador == pisoQuebrado) {
+        alert("O vidro " + escolhaJogador + " quebrou! Fim de jogo!")
         rodada = 1000
-    }
-    else {
+        venceu = 1
+    } else {
         alert("Passou! O piso quebrado estava na ponte: " + pisoQuebrado)
     }
-    if(rodada == 4){
-        alert("Parabéns! Você venceu!")
-    }
-    rodada = rodada + 1
+}
+if (venceu == 0) {
+    alert("Parabéns! Você venceu!")
 }
